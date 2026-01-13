b73ad4374c06e57ed3a2b4e9c7f455bb3037c8ea
Sai Pranavreddy <edulapranav@gmail.com>
2026-01-13 12:00:05 +0530

    Create Main.java

---
+++ src/main/java/com/pranav/guessthenumber/Main.java
@@ -0,0 +1,8 @@
+package com.pranav.guessthenumber;
+
+public class Main {
+    public static void main(String[] args) {
+        GuessTheNumberGame game = new GuessTheNumberGame();
+        game.startGame();
+    }
+}
